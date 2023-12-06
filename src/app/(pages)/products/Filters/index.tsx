b618820ec6a232c44c12837f6a2b5aed80a47835
ParvinMusayev <parvinmusayev1@gmail.com>
2023-12-06 22:09:29 +0400

    created products page

---
+++ src/app/(pages)/products/Filters/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import classes from './index.module.scss'
+import { Category } from '../../../../payload/payload-types'
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  return (
+    <div>Filters</div>
+  )
+}
+
+export default Filters
